f60343884ae85a12784b198e91fd4bbe752963f4
Atif Colo <atif.c@whoaverse.com>
2014-07-11 15:44:08 +0200

    added more experimental APIs, updated dependencies

--- Whoaverse/Whoaverse/App_Start/WebApiConfig.cs
+++ Whoaverse/Whoaverse/App_Start/WebApiConfig.cs
@@ -14,11 +14,18 @@ namespace Whoaverse
             // set default API response to JSON
             configuration.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
+            // configure routes
             configuration.Routes.MapHttpRoute("API list default subverses", "api/defaultsubverses",
                 new { controller = "WebApi", action = "DefaultSubverses" });
 
             configuration.Routes.MapHttpRoute("API list banned hostnames", "api/bannedhostnames",
                 new { controller = "WebApi", action = "BannedHostnames" });
+
+            configuration.Routes.MapHttpRoute("API list top 200 subverses", "api/top200subverses",
+                new { controller = "WebApi", action = "Top200Subverses" });
+
+            configuration.Routes.MapHttpRoute("API frontpage", "api/frontpage",
+                new { controller = "WebApi", action = "Frontpage" });  
         }
     }
 }
--- Whoaverse/Whoaverse/Controllers/WebApiController.cs
+++ Whoaverse/Whoaverse/Controllers/WebApiController.cs
@@ -53,5 +53,52 @@ namespace Whoaverse.Controllers
             return tmpList;
         }
 
+        // GET api/top200subverses
+        [System.Web.Http.HttpGet]
+        public IEnumerable<string> Top200Subverses()
+        {
+            var top200Subverses = db.Subverses.OrderByDescending(s => s.subscribers).ToList();
+
+            List<string> resultList = new List<string>();
+            foreach (var item in top200Subverses)
+            {
+                resultList.Add(
+                    "Name: " + item.name + "," +
+                    "Description: " + item.description + "," +
+                    "Subscribers: " + item.subscribers + "," +
+                    "Created: " + item.creation_date
+                    );
+            }
+
+            return resultList;
+        }
+
+        // GET api/frontpage
+        [System.Web.Http.HttpGet]
+        public IEnumerable<string> Frontpage()
+        {
+            //get only submissions from default subverses, order by rank
+            var frontpageSubmissions = (from message in db.Messages
+                               join defaultsubverse in db.Defaultsubverses on message.Subverse equals defaultsubverse.name
+                               where message.Name != "deleted"
+                               select message)
+                               .Distinct()
+                               .OrderByDescending(s => s.Rank).Take(100).ToList();
+
+            List<string> resultList = new List<string>();
+            foreach (var item in frontpageSubmissions)
+            {
+                resultList.Add(
+                    "Type: " + item.Type + "," +
+                    "Title: " + item.Title + "," +
+                    "Link description: " + item.Linkdescription + "," +
+                    "Date: " + item.Date + "," +
+                    "Comments: " + item.Comments.Count() + "," +
+                    "Author: " + item.Name
+                    );
+            }
+
+            return resultList;
+        }
     }
 }
