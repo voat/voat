67e1438ecac167a781480c108e809d6297bc0b42
AngraManyu <angragit@outlook.com>
2014-07-13 02:20:23 -0600

    Added Email Registering for Issue #5

---
+++ Whoaverse/Whoaverse/Utils/ConstantUtility.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Whoaverse.Utils
+{
+    public static class ConstantUtility
+    {
+        public const string EmailRegex = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
+    }
+}
