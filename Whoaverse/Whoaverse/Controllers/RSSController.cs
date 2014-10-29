9b48018ab0325c5ea24c137b9d3049a437bfdbf5
Atif Colo <atif.c@whoaverse.com>
2014-10-29 14:04:55 +0100

    rsscontroller refactoring

--- Whoaverse/Whoaverse/Controllers/RSSController.cs
+++ Whoaverse/Whoaverse/Controllers/RSSController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.ServiceModel.Syndication;
 using System.Web.Mvc;
@@ -8,23 +9,22 @@ using Whoaverse.Utils;
 
 namespace Whoaverse.Controllers
 {
-    public class RSSController : Controller
+    public class RssController : Controller
     {
-        private whoaverseEntities db = new whoaverseEntities();
+        private readonly whoaverseEntities _db = new whoaverseEntities();
         
         // GET: rss/{subverseName}
-        public ActionResult RSS(string subverseName)
+        public ActionResult Rss(string subverseName)
         {
-            List<Message> submissions = new List<Message>();
-            Random rnd = new Random();
+            var submissions = new List<Message>();
 
             if (subverseName != null)
             {
                 // return only frontpage submissions from a given subverse
-                Subverse subverse = db.Subverses.Find(subverseName);
+                var subverse = _db.Subverses.Find(subverseName);
                 if (subverse != null)
                 {
-                    submissions = (from message in db.Messages
+                    submissions = (from message in _db.Messages
                                    where message.Name != "deleted" && message.Subverse == subverse.name
                                    select message)
                                    .OrderByDescending(s => s.Rank)
@@ -35,38 +35,41 @@ namespace Whoaverse.Controllers
             else
             {
                 // return site-wide frontpage submissions
-                submissions = (from message in db.Messages
+                submissions = (from message in _db.Messages
                                where message.Name != "deleted"
-                               join defaultsubverse in db.Defaultsubverses on message.Subverse equals defaultsubverse.name
+                               join defaultsubverse in _db.Defaultsubverses on message.Subverse equals defaultsubverse.name
                                select message)
                                .OrderByDescending(s => s.Rank)
                                .Take(25)
                                .ToList();
             }
 
-            SyndicationFeed feed = new SyndicationFeed("WhoaVerse", "The frontpage of the Universe", new Uri("http://www.whoaverse.com"));
-            feed.Language = "en-US";
-            feed.ImageUrl = new Uri("http://" + System.Web.HttpContext.Current.Request.Url.Authority + "/Graphics/whoaverse_padded.png");
+            var feed = new SyndicationFeed("WhoaVerse", "The frontpage of the Universe", new Uri("http://www.whoaverse.com"))
+            {
+                Language = "en-US",
+                ImageUrl =
+                    new Uri("http://" + System.Web.HttpContext.Current.Request.Url.Authority +
+                            "/Graphics/whoaverse_padded.png")
+            };
 
-            List<SyndicationItem> feedItems = new List<SyndicationItem>();
+            var feedItems = new List<SyndicationItem>();
 
             foreach (var submission in submissions)
             {
                 var commentsUrl = new Uri("http://" + System.Web.HttpContext.Current.Request.Url.Authority + "/v/" + submission.Subverse + "/comments/" + submission.Id);
                 var subverseUrl = new Uri("http://" + System.Web.HttpContext.Current.Request.Url.Authority + "/v/" + submission.Subverse);
 
-                string thumbnailUrl = "";
-                string authorName = submission.Name;
+                var authorName = submission.Name;
 
                 if (submission.Type == 1)
                 {
                     // message type submission
                     if (submission.Anonymized || submission.Subverses.anonymized_mode)
                     {
-                        authorName = submission.Id.ToString();
+                        authorName = submission.Id.ToString(CultureInfo.InvariantCulture);
                     }
 
-                    SyndicationItem item = new SyndicationItem(
+                    var item = new SyndicationItem(
                     submission.Title,
                     submission.MessageContent + "</br>" + "Submitted by " + "<a href='u/" + authorName + "'>" + authorName + "</a> to <a href='" + subverseUrl + "'>" + submission.Subverse + "</a> | <a href='" + commentsUrl + "'>" + submission.Comments.Count() + " comments",
                     commentsUrl,
@@ -82,14 +85,14 @@ namespace Whoaverse.Controllers
 
                     if (submission.Anonymized || submission.Subverses.anonymized_mode)
                     {
-                        authorName = submission.Id.ToString();
+                        authorName = submission.Id.ToString(CultureInfo.InvariantCulture);
                     }
 
                     // add a thumbnail if submission has one
                     if (submission.Thumbnail != null)
                     {
-                        thumbnailUrl = new Uri("http://" + System.Web.HttpContext.Current.Request.Url.Authority + "/Thumbs/" + submission.Thumbnail).ToString();
-                        SyndicationItem item = new SyndicationItem(
+                        var thumbnailUrl = new Uri("http://" + System.Web.HttpContext.Current.Request.Url.Authority + "/Thumbs/" + submission.Thumbnail).ToString();
+                        var item = new SyndicationItem(
                                                 submission.Linkdescription,
                                                 "<a xmlns='http://www.w3.org/1999/xhtml' href='" + commentsUrl + "'><img title='" + submission.Linkdescription + "' alt='" + submission.Linkdescription + "' src='" + thumbnailUrl + "' /></a>" +
                                                 "</br>" +
@@ -103,7 +106,7 @@ namespace Whoaverse.Controllers
                     }
                     else
                     {
-                        SyndicationItem item = new SyndicationItem(
+                        var item = new SyndicationItem(
                                                 submission.Linkdescription,
                                                 "Submitted by " + "<a href='u/" + authorName + "'>" + authorName + "</a> to <a href='" + subverseUrl + "'>" + submission.Subverse + "</a> | <a href='" + commentsUrl + "'>" + submission.Comments.Count() + " comments",
                                                 commentsUrl,
