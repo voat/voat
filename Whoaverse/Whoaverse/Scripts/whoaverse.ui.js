f4ff58634e141618eedd4abee94e2eb7e3d238eb
Craig Barabas <craig.barabas@miosoft.com>
2015-02-15 22:22:04 -0600

    Added backdrop and some animations too the sidebar

--- Whoaverse/Whoaverse/Scripts/whoaverse.ui.js
+++ Whoaverse/Whoaverse/Scripts/whoaverse.ui.js
@@ -792,11 +792,25 @@ var MenuHandler = function() {
     var sidebar = $(".side");
     //The button to shwo the sidebar when mobile
     var showMenuBtn = $("#show-menu-button");
+    //The background to the sidebar
+    var modalBg = $("#modal-background");
     
     //Add a click listener
-    showMenuBtn.on("click", function() {
+    showMenuBtn.on("click", function () {
+        modalBg.toggleClass("show-mobile-sidebar");
+        showMenuBtn.toggleClass("show-mobile-sidebar");
         sidebar.toggleClass("show-mobile-sidebar");
     });
+
+    modalBg.on("click", function () {
+        hideSidebar();
+    });
+
+    function hideSidebar () {
+        modalBg.toggleClass("show-mobile-sidebar", false);
+        showMenuBtn.toggleClass("show-mobile-sidebar", false);
+        sidebar.toggleClass("show-mobile-sidebar", false);
+    }
 
     //Media query listener to remove class when resized to desktop size
     var mql = window.matchMedia("(min-width: 870px)");
@@ -804,7 +818,7 @@ var MenuHandler = function() {
 
     function handleMediaQuery(mql) {
         if (mql.matches) {
-            sidebar.toggleClass("show-mobile-sidebar", false);
+            hideSidebar();
         }
     }
 }
