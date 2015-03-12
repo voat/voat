90c497e5d205671be9263a2eed957f03b928898b
Atif Colo <atif.c@whoaverse.com>
2015-03-12 07:54:23 +0100

    get unique submissions based on message.subverse from all instead of from already selected top 5 submissions for most visited in last 24 hours sidebar

--- Whoaverse/Whoaverse/Controllers/SubversesController.cs
+++ Whoaverse/Whoaverse/Controllers/SubversesController.cs
@@ -1579,7 +1579,7 @@ namespace Voat.Controllers
                                                                                 join subverse in _db.Subverses on message.Subverse equals subverse.name
                                                                                 where message.Name != "deleted" && subverse.private_subverse != true && subverse.rated_adult == false && message.Date >= startDate && message.Date <= DateTime.Now
                                                                                 where !(from bu in _db.Bannedusers select bu.Username).Contains(message.Name)
-                                                                                select message).OrderByDescending(s => s.Views).Take(5).DistinctBy(m => m.Subverse).AsQueryable().AsNoTracking();
+                                                                                select message).DistinctBy(m => m.Subverse).OrderByDescending(s => s.Views).Take(5).AsQueryable().AsNoTracking();
 
             return sfwSubmissionsFromAllSubversesByViews24Hours;
         }
