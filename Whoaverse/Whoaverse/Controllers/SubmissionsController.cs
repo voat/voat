ac2c7c9119f4bb212260be9d5e551e5dce1a4107
DanielFlamino <danielflamino96@gmail.com>
2015-04-10 23:41:08 +0100

    ok this one I missed

--- Whoaverse/Whoaverse/Controllers/SubmissionsController.cs
+++ Whoaverse/Whoaverse/Controllers/SubmissionsController.cs
@@ -233,7 +233,7 @@ namespace Voat.Controllers
                             submissionToDelete.Name,
                             "Your submission has been deleted by a moderator",
                             "Your [submission](/v/" + submissionToDelete.Subverse + "/comments/" + submissionToDelete.Id + ") has been deleted by: " +
-                            "[" + User.Identity.Name + "](/user/" + User.Identity.Name + ")" + " at " + DateTime.Now + "  " + Environment.NewLine +
+                            "/u/" + User.Identity.Name + User.Identity.Name + ")" + " at " + DateTime.Now + "  " + Environment.NewLine +
                             "Original submission content was: " + Environment.NewLine +
                             "---" + Environment.NewLine +
                             "Link description: " + submissionToDelete.Linkdescription + ", " + Environment.NewLine +
