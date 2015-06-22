6d43b8fb7a0698c512a5299265f45f3fbeda53d5
Patryk Cwiek <patryk.cwiek@gmail.com>
2015-06-22 14:05:30 +0200

    #473 Added comment count query with tests

---
+++ Whoaverse/Tests/Query.Tests/CommentQueries.cs
@@ -0,0 +1,86 @@
+﻿namespace Query.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Voat.Models;
+    using Voat.Queries.Comments;
+    using Xunit;
+
+    [Trait("Category", "Semi-integration"), Trait("Subcategory", "Queries")]
+    public class CommentQueries : IDisposable
+    {
+         private readonly whoaverseEntities dbContext;
+
+        public CommentQueries()
+        {
+            var effortConnection = Effort.EntityConnectionFactory.CreateTransient(EntityString.Value);
+            dbContext = new whoaverseEntities(effortConnection);
+
+            dbContext.Set<Subverse>().Add(new Subverse
+            {
+                title = "test",
+                name = "test"
+            });
+
+            var submissions = new[]
+            {
+                dbContext.Set<Message>().Add(new Message
+                {
+                    Name = "test message",
+                    Subverse = "test",
+                    Id = 1
+
+                }),
+                dbContext.Set<Message>().Add(new Message
+                {
+                    Name = "another test",
+                    Subverse = "test",
+                    Id = 2
+                })
+            };
+
+            dbContext.SaveChanges();
+
+            dbContext.Set<Comment>().Add(new Comment
+            {
+                Id = 1,
+                Message = submissions[0],
+                Name = "test2",
+                CommentContent = "Empty"
+            });
+
+            dbContext.Set<Comment>().Add(new Comment
+            {
+                Id = 2,
+                Message = submissions[0],
+                Name = "johnny",
+                CommentContent = "Empty"
+            });
+
+            dbContext.Set<Comment>().Add(new Comment
+            {
+                Id = 3,
+                Message = submissions[1],
+                Name = "test",
+                CommentContent = "Empty"
+            });
+            dbContext.SaveChanges();
+        }
+
+        [Theory(DisplayName = "Comment count should be correctly retrieved for submission.")]
+        [InlineData(1, 2)]
+        [InlineData(2, 1)]
+        [InlineData(4, 0)]
+        public async Task CommentCountRetrieval(int submissionId, int expectedCount)
+        {
+            var result = await dbContext.Set<Comment>().GetCommentCountAsync(submissionId);
+
+            Assert.Equal(expectedCount, result);
+        }
+
+        public void Dispose()
+        {
+            dbContext.Dispose();
+        }
+    }
+}
---
+++ Whoaverse/Whoaverse/Queries/Comments/CommentCount.cs
@@ -0,0 +1,15 @@
+﻿namespace Voat.Queries.Comments
+{
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Models;
+
+    public static class CommentCount
+    {
+        public static Task<int> GetCommentCountAsync(this IQueryable<Comment> query, int submissionId)
+        {
+            return query.Where(c => c.MessageId == submissionId && c.Name != "deleted").CountAsync();
+        }
+    }
+}
