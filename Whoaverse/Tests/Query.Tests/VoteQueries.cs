83dcd6ede93c72d0fa4eb87992727207dc3a859e
Patryk Cwiek <patryk.cwiek@gmail.com>
2015-06-22 15:44:53 +0200

    Merged submission and comment voting queries' tests into one test fixture.

--- Whoaverse/Tests/Query.Tests/VoteQueries.cs
+++ Whoaverse/Tests/Query.Tests/VoteQueries.cs
@@ -1,4 +1,4 @@
-﻿namespace Query.Tests.Voting
+﻿namespace Query.Tests
 {
     using System;
     using System.Data.Entity;
@@ -8,15 +8,15 @@
     using Xunit;
 
     [Trait("Category", "Semi-integration"), Trait("Subcategory", "Queries")]
-    public class CommentVoteQueries : IDisposable
+    public class VoteQueries : IDisposable
     {
         private readonly DbContext dbContext;
 
-        public CommentVoteQueries()
+        public VoteQueries()
         {
             var effortConnection = Effort.EntityConnectionFactory.CreateTransient(EntityString.Value);
             dbContext = new whoaverseEntities(effortConnection);
-
+            #region Comment vote set up
             dbContext.Set<Subverse>().Add(new Subverse
             {
                 title = "test",
@@ -75,8 +75,82 @@
                 VoteStatus = -1,
                 CommentId = 1
             });
+            #endregion
+
+            #region Submission vote setup
+            dbContext.Set<Message>().Add(new Message
+            {
+                Id = 1,
+                Name = "abc",
+                Title = "test",
+                Subverse = "test"
+            });
+
+            dbContext.Set<Message>().Add(new Message
+            {
+                Id = 2,
+                Name = "abc",
+                Title = "test",
+                Subverse = "test"
+            });
+
+            dbContext.Set<Message>().Add(new Message
+            {
+                Id = 3,
+                Name = "abc",
+                Title = "test",
+                Subverse = "test"
+            });
+
+            dbContext.Set<Votingtracker>().Add(new Votingtracker
+            {
+                Id = 1,
+                UserName = "testUser",
+                MessageId = 1,
+                VoteStatus = 1
+            });
+
+            dbContext.Set<Votingtracker>().Add(new Votingtracker
+            {
+                Id = 2,
+                UserName = "testUser",
+                MessageId = 2,
+                VoteStatus = 0
+            });
+
+            dbContext.Set<Votingtracker>().Add(new Votingtracker
+            {
+                Id = 3,
+                UserName = "testUser2",
+                VoteStatus = 1,
+                MessageId = 2
+            });
+
+            dbContext.Set<Votingtracker>().Add(new Votingtracker
+            {
+                Id = 4,
+                UserName = "testUser2",
+                VoteStatus = -1,
+                MessageId = 1
+            });
+            #endregion
 
             dbContext.SaveChanges();
+        }
+
+
+        [Theory(DisplayName = "Query to check for current vote should return correct vote status.")]
+        [InlineData("testUser", 1, VoteStatus.Upvoted)]
+        [InlineData("testUser", 2, VoteStatus.None)]
+        [InlineData("testUser2", 2, VoteStatus.Upvoted)]
+        [InlineData("testUser2", 1, VoteStatus.Downvoted)]
+        [InlineData("testUser", 3, VoteStatus.None)]
+        [InlineData("testUser2", 3, VoteStatus.None)]
+        public async Task CheckingSubmissionForUserVote(string userName, int commentId,
+            VoteStatus expectedStatus)
+        {
+            var result = await dbContext.Set<Votingtracker>().CheckSubmissionForVoteAsync(userName, commentId);
+            Assert.Equal(expectedStatus, result);
         }
 
         [Theory(DisplayName = "Query to check for current vote should return correct vote status.")]
@@ -92,6 +166,7 @@
             var result = await dbContext.Set<Commentvotingtracker>().CheckCommentForVoteAsync(userName, commentId);
             Assert.Equal(expectedStatus, result);
         }
+
 
         public void Dispose()
         {
