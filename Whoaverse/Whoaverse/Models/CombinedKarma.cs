51b322cb9b0fd3b7e8130a56bd78c34322841e86
Patryk Cwiek <patryk.cwiek@gmail.com>
2015-06-24 10:37:56 +0200

    Combined karma model should now have structural equality.

--- Whoaverse/Whoaverse/Models/CombinedKarma.cs
+++ Whoaverse/Whoaverse/Models/CombinedKarma.cs
@@ -1,7 +1,52 @@
 ﻿namespace Voat.Models
 {
-    public class CombinedKarma
+    using System;
+
+    public sealed class CombinedKarma : IEquatable<CombinedKarma>
     {
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <returns>
+        /// true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
+        /// </returns>
+        /// <param name="other">An object to compare with this object.</param>
+        public bool Equals(CombinedKarma other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return LinkKarma == other.LinkKarma && CommentKarma == other.CommentKarma;
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>.
+        /// </summary>
+        /// <returns>
+        /// true if the specified object  is equal to the current object; otherwise, false.
+        /// </returns>
+        /// <param name="obj">The object to compare with the current object. </param>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((CombinedKarma) obj);
+        }
+
+        /// <summary>
+        /// Serves as a hash function for a particular type. 
+        /// </summary>
+        /// <returns>
+        /// A hash code for the current <see cref="T:System.Object"/>.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (LinkKarma*397) ^ CommentKarma;
+            }
+        }
+
         public CombinedKarma(int linkKarma, int commentKarma)
         {
             LinkKarma = linkKarma;
