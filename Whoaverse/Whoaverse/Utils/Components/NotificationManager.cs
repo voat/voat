482ed2dd2bcf0daa9fcd14a39fdfd28841fd2bce
Robert Hencke <robert.hencke@gmail.com>
2015-07-09 01:07:20 -0400

    Ensure _db is disposed after use.

--- Whoaverse/Whoaverse/Utils/Components/NotificationManager.cs
+++ Whoaverse/Whoaverse/Utils/Components/NotificationManager.cs
@@ -116,108 +116,109 @@ namespace Voat.Utils.Components
 
         public static async Task SendCommentNotification(Comment comment)
         {
-
-            whoaverseEntities _db = new whoaverseEntities();
-            Random _rnd = new Random();
-
-            if (comment.ParentId != null && comment.CommentContent != null)
+            using (var _db = new whoaverseEntities())
             {
-                // find the parent comment and its author
-                var parentComment = _db.Comments.Find(comment.ParentId);
-                if (parentComment != null)
+                Random _rnd = new Random();
+
+                if (comment.ParentId != null && comment.CommentContent != null)
                 {
-                    // check if recipient exists
-                    if (User.UserExists(parentComment.Name))
+                    // find the parent comment and its author
+                    var parentComment = _db.Comments.Find(comment.ParentId);
+                    if (parentComment != null)
                     {
-                        // do not send notification if author is the same as comment author
-                        if (parentComment.Name != HttpContext.Current.User.Identity.Name)
+                        // check if recipient exists
+                        if (User.UserExists(parentComment.Name))
                         {
-                            // send the message
-
-                            var commentMessage = _db.Messages.Find(comment.MessageId);
-                            if (commentMessage != null)
+                            // do not send notification if author is the same as comment author
+                            if (parentComment.Name != HttpContext.Current.User.Identity.Name)
                             {
-                                var commentReplyNotification = new Commentreplynotification();
-                                commentReplyNotification.CommentId = comment.Id;
-                                commentReplyNotification.SubmissionId = commentMessage.Id;
-                                commentReplyNotification.Recipient = parentComment.Name;
-                                if (parentComment.Message.Anonymized || parentComment.Message.Subverses.anonymized_mode)
+                                // send the message
+
+                                var commentMessage = _db.Messages.Find(comment.MessageId);
+                                if (commentMessage != null)
                                 {
-                                    commentReplyNotification.Sender = _rnd.Next(10000, 20000).ToString(CultureInfo.InvariantCulture);
+                                    var commentReplyNotification = new Commentreplynotification();
+                                    commentReplyNotification.CommentId = comment.Id;
+                                    commentReplyNotification.SubmissionId = commentMessage.Id;
+                                    commentReplyNotification.Recipient = parentComment.Name;
+                                    if (parentComment.Message.Anonymized || parentComment.Message.Subverses.anonymized_mode)
+                                    {
+                                        commentReplyNotification.Sender = _rnd.Next(10000, 20000).ToString(CultureInfo.InvariantCulture);
+                                    }
+                                    else
+                                    {
+                                        commentReplyNotification.Sender = HttpContext.Current.User.Identity.Name;
+                                    }
+                                    commentReplyNotification.Body = comment.CommentContent;
+                                    commentReplyNotification.Subverse = commentMessage.Subverse;
+                                    commentReplyNotification.Status = true;
+                                    commentReplyNotification.Timestamp = DateTime.Now;
+
+                                    // self = type 1, url = type 2
+                                    commentReplyNotification.Subject = parentComment.Message.Type == 1 ? parentComment.Message.Title : parentComment.Message.Linkdescription;
+
+                                    _db.Commentreplynotifications.Add(commentReplyNotification);
+
+                                    await _db.SaveChangesAsync();
+
+                                    // get count of unread notifications
+                                    int unreadNotifications = User.UnreadTotalNotificationsCount(commentReplyNotification.Recipient);
+
+                                    // send SignalR realtime notification to recipient
+                                    var hubContext = GlobalHost.ConnectionManager.GetHubContext<MessagingHub>();
+                                    hubContext.Clients.User(commentReplyNotification.Recipient).setNotificationsPending(unreadNotifications);
                                 }
-                                else
-                                {
-                                    commentReplyNotification.Sender = HttpContext.Current.User.Identity.Name;
-                                }
-                                commentReplyNotification.Body = comment.CommentContent;
-                                commentReplyNotification.Subverse = commentMessage.Subverse;
-                                commentReplyNotification.Status = true;
-                                commentReplyNotification.Timestamp = DateTime.Now;
-
-                                // self = type 1, url = type 2
-                                commentReplyNotification.Subject = parentComment.Message.Type == 1 ? parentComment.Message.Title : parentComment.Message.Linkdescription;
-
-                                _db.Commentreplynotifications.Add(commentReplyNotification);
-
-                                await _db.SaveChangesAsync();
-
-                                // get count of unread notifications
-                                int unreadNotifications = User.UnreadTotalNotificationsCount(commentReplyNotification.Recipient);
-
-                                // send SignalR realtime notification to recipient
-                                var hubContext = GlobalHost.ConnectionManager.GetHubContext<MessagingHub>();
-                                hubContext.Clients.User(commentReplyNotification.Recipient).setNotificationsPending(unreadNotifications);
                             }
                         }
                     }
                 }
-            }
-            else
-            {
-                // comment reply is sent to a root comment which has no parent id, trigger post reply notification
-                var commentMessage = _db.Messages.Find(comment.MessageId);
-                if (commentMessage != null)
+                else
                 {
-                    // check if recipient exists
-                    if (User.UserExists(commentMessage.Name))
+                    // comment reply is sent to a root comment which has no parent id, trigger post reply notification
+                    var commentMessage = _db.Messages.Find(comment.MessageId);
+                    if (commentMessage != null)
                     {
-                        // do not send notification if author is the same as comment author
-                        if (commentMessage.Name != HttpContext.Current.User.Identity.Name)
+                        // check if recipient exists
+                        if (User.UserExists(commentMessage.Name))
                         {
-                            // send the message
-                            var postReplyNotification = new Postreplynotification();
-
-                            postReplyNotification.CommentId = comment.Id;
-                            postReplyNotification.SubmissionId = commentMessage.Id;
-                            postReplyNotification.Recipient = commentMessage.Name;
-
-                            if (commentMessage.Anonymized || commentMessage.Subverses.anonymized_mode)
+                            // do not send notification if author is the same as comment author
+                            if (commentMessage.Name != HttpContext.Current.User.Identity.Name)
                             {
-                                postReplyNotification.Sender = _rnd.Next(10000, 20000).ToString(CultureInfo.InvariantCulture);
+                                // send the message
+                                var postReplyNotification = new Postreplynotification();
+
+                                postReplyNotification.CommentId = comment.Id;
+                                postReplyNotification.SubmissionId = commentMessage.Id;
+                                postReplyNotification.Recipient = commentMessage.Name;
+
+                                if (commentMessage.Anonymized || commentMessage.Subverses.anonymized_mode)
+                                {
+                                    postReplyNotification.Sender = _rnd.Next(10000, 20000).ToString(CultureInfo.InvariantCulture);
+                                }
+                                else
+                                {
+                                    postReplyNotification.Sender = HttpContext.Current.User.Identity.Name;
+                                }
+
+                                postReplyNotification.Body = comment.CommentContent;
+                                postReplyNotification.Subverse = commentMessage.Subverse;
+                                postReplyNotification.Status = true;
+                                postReplyNotification.Timestamp = DateTime.Now;
+
+                                // self = type 1, url = type 2
+                                postReplyNotification.Subject = commentMessage.Type == 1 ? commentMessage.Title : commentMessage.Linkdescription;
+
+                                _db.Postreplynotifications.Add(postReplyNotification);
+
+                                await _db.SaveChangesAsync();
+
+                                // get count of unread notifications
+                                int unreadNotifications = User.UnreadTotalNotificationsCount(postReplyNotification.Recipient);
+
+                                // send SignalR realtime notification to recipient
+                                var hubContext = GlobalHost.ConnectionManager.GetHubContext<MessagingHub>();
+                                hubContext.Clients.User(postReplyNotification.Recipient).setNotificationsPending(unreadNotifications);
                             }
-                            else
-                            {
-                                postReplyNotification.Sender = HttpContext.Current.User.Identity.Name;
-                            }
-
-                            postReplyNotification.Body = comment.CommentContent;
-                            postReplyNotification.Subverse = commentMessage.Subverse;
-                            postReplyNotification.Status = true;
-                            postReplyNotification.Timestamp = DateTime.Now;
-
-                            // self = type 1, url = type 2
-                            postReplyNotification.Subject = commentMessage.Type == 1 ? commentMessage.Title : commentMessage.Linkdescription;
-
-                            _db.Postreplynotifications.Add(postReplyNotification);
-
-                            await _db.SaveChangesAsync();
-
-                            // get count of unread notifications
-                            int unreadNotifications = User.UnreadTotalNotificationsCount(postReplyNotification.Recipient);
-
-                            // send SignalR realtime notification to recipient
-                            var hubContext = GlobalHost.ConnectionManager.GetHubContext<MessagingHub>();
-                            hubContext.Clients.User(postReplyNotification.Recipient).setNotificationsPending(unreadNotifications);
                         }
                     }
                 }
