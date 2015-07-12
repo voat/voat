239a049fbaf278d3841798d412ecb65e8518a253
PuttItOut <github.com@not.mrperfect.us>
2015-07-11 17:17:57 -0700

    Path Changes

---
+++ Voat/Voat.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Voat.Tests")]
+[assembly: AssemblyDescription("Unit Tests for Voat Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Voat GmbH")]
+[assembly: AssemblyProduct("Voat")]
+[assembly: AssemblyCopyright("Copyright © voat.co 2015")]
+[assembly: AssemblyTrademark("voat")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6df24492-bedc-43d6-9a3a-a225d889d176")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
