32a0f5f2f2fd5abce69f4874400538e532f4ce14
PuttItOut <github.com@not.mrperfect.us>
2015-12-05 14:36:01 -0700

    Highest Rank in Subverse Cache Logic Stub

--- Voat/Voat.Business/Utilities/Ranking.cs
+++ Voat/Voat.Business/Utilities/Ranking.cs
@@ -13,11 +13,57 @@ All Rights Reserved.
 */
 
 using System;
+using Voat.Data.Models;
+using System.Linq;
 
 namespace Voat.Utilities
 {
+    //Simple object to store highest rank because we need to know submission ID to update it in cache
+    public class HighestRank
+    {
+        public double Rank { get; set; }
+        public int SubmissionID { get; set; }
+    }
+
     public static class Ranking
     {
+        private static string GetHighestRankingCacheKey(string subverse)
+        {
+            return String.Format("subverse.{0}.highest-rank", subverse).ToLower();
+        }
+        public static HighestRank GetSubverseHighestRanking(string subverse)
+        {
+            var highestRank =  CacheHandler.Register(GetHighestRankingCacheKey(subverse), new Func<HighestRank>(() => {
+                using (var db = new voatEntities())
+                {
+                    var submission = db.Submissions.OrderByDescending(x => x.Rank).Where(x => x.Subverse == subverse).FirstOrDefault();
+                    return new HighestRank() { Rank = submission.Rank, SubmissionID = submission.ID };
+                }
+            }), TimeSpan.FromMinutes(30));
+
+            return highestRank;
+
+        }
+        public static void UpdateSubverseHighestRanking(string subverse, int submissionID, double newRank)
+        {
+            var highestRankCacheEntry = GetSubverseHighestRanking(subverse);
+
+            if (highestRankCacheEntry != null)
+            {
+                if (highestRankCacheEntry.Rank < newRank)
+                {
+                    if (highestRankCacheEntry.SubmissionID != submissionID)
+                    {
+                        highestRankCacheEntry.SubmissionID = submissionID;
+                    }
+
+                    highestRankCacheEntry.Rank = newRank;
+
+                    CacheHandler.Replace(GetHighestRankingCacheKey(subverse), new Func<HighestRank, HighestRank>(current => highestRankCacheEntry));
+                }
+            }
+        }
+
         // re-rank a submission
         public static double CalculateNewRank(double currentRank, double submissionAge, double score)
         {
@@ -28,9 +74,10 @@ namespace Voat.Utilities
             return double.IsNaN(newRank) ? 0 : Math.Round(newRank, 7);
         }
 
-        public static double CalculateNewRelativeRank(double rank, double subCtr)
+        public static double CalculateNewRelativeRank(double rank, double highestRankInSubverse)
         {
-            double relativeRank = rank/subCtr;
+            double relativeRank = rank / highestRankInSubverse;
+
             return double.IsNaN(relativeRank) ? 0 : Math.Round(relativeRank, 7);
         }
     }
