a980e05b2286ab2eb11ce4e8b84f2f75a17a4c15
PuttItOut <github.com@not.mrperfect.us>
2016-04-16 11:10:37 -0700

    Stub out for not rendering ads for donors

--- Voat/Voat.UI/Controllers/AdController.cs
+++ Voat/Voat.UI/Controllers/AdController.cs
@@ -14,28 +14,39 @@ namespace Voat.Controllers
             using (var db = new voatEntities())
             {
                 // TODO: check if currently logged in user is a donor or subscriber and has opted out of ads, if so, do not display any ads whatsoever
+                var renderAd = true;
 
-                var ad = (from x in db.Ads
-                          where
-                          ((subverse != null && x.Subverse.Equals(subverse, StringComparison.InvariantCultureIgnoreCase) || (subverse == null && x.Subverse == null)))
-                          && (x.EndDate >= DateTime.Now && x.StartDate <= DateTime.Now)
-                          orderby x.Subverse descending
-                          select x).FirstOrDefault();
-
-                if (ad != null)
+                if (renderAd)
                 {
-                    var adModel = new AdViewModel
+
+                    var ad = (from x in db.Ads
+                              where
+                              ((subverse != null && x.Subverse.Equals(subverse, StringComparison.InvariantCultureIgnoreCase) || (subverse == null && x.Subverse == null)))
+                              && (x.EndDate >= DateTime.Now && x.StartDate <= DateTime.Now)
+                              orderby x.Subverse descending
+                              select x).FirstOrDefault();
+
+                    if (ad != null)
                     {
-                        Name = ad.Name,
-                        DestinationUrl = ad.DestinationUrl,
-                        Description = ad.Description,
-                        GraphicUrl = ad.GraphicUrl
-                    };
-                    return View("_Ad", adModel);
+                        var adModel = new AdViewModel
+                        {
+                            Name = ad.Name,
+                            DestinationUrl = ad.DestinationUrl,
+                            Description = ad.Description,
+                            GraphicUrl = ad.GraphicUrl
+                        };
+                        return View("_Ad", adModel);
+                    }
+
+                    // no running ads found, render ad placeholder instead
+                    return RenderPlaceholder();
+                }
+                else
+                {
+                    return View("_Ad"); //a null model will prevent ad from rendering any html
                 }
             }
-            // no running ads found, render ad placeholder instead
-            return RenderPlaceholder();
+            
         }
 
         public ViewResult RenderPlaceholder()
