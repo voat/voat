a1ddd24395810bdc484ea8099a40c17ff9a90221
PuttItOut <github.com@not.mrperfect.us>
2016-04-15 19:39:06 -0700

    Formatting of desc, centering of image, query reduction

--- Voat/Voat.UI/Controllers/AdController.cs
+++ Voat/Voat.UI/Controllers/AdController.cs
@@ -15,40 +15,25 @@ namespace Voat.Controllers
             {
                 // TODO: check if currently logged in user is a donor or subscriber and has opted out of ads, if so, do not display any ads whatsoever
 
-                // get currently running ad for target subverse
-                if (subverse != null)
-                {
-                    var subverseAd = db.Ads.FirstOrDefault(x => x.Subverse.Equals(subverse, StringComparison.InvariantCultureIgnoreCase) && x.EndDate > DateTime.Now);
-                    if (subverseAd != null)
-                    {
-                        var adModel = new AdViewModel
-                        {
-                            Name = subverseAd.Name,
-                            DestinationUrl = subverseAd.DestinationUrl,
-                            Description = subverseAd.Description,
-                            GraphicUrl = subverseAd.GraphicUrl
-                        };
+                var ad = (from x in db.Ads
+                          where
+                          ((subverse != null && x.Subverse.Equals(subverse, StringComparison.InvariantCultureIgnoreCase) || (subverse == null && x.Subverse == null)))
+                          && (x.EndDate >= DateTime.Now && x.StartDate <= DateTime.Now)
+                          orderby x.Subverse descending
+                          select x).FirstOrDefault();
 
-                        return View("_Ad", adModel);
-                    }
-                }
-
-                // no running ad for target subverse found, get currently running global ad instead
-                var globalAd = db.Ads.FirstOrDefault(x => x.EndDate > DateTime.Now && x.Subverse == null);
-                if (globalAd != null)
+                if (ad != null)
                 {
                     var adModel = new AdViewModel
                     {
-                        Name = globalAd.Name,
-                        DestinationUrl = globalAd.DestinationUrl,
-                        Description = globalAd.Description,
-                        GraphicUrl = globalAd.GraphicUrl
+                        Name = ad.Name,
+                        DestinationUrl = ad.DestinationUrl,
+                        Description = ad.Description,
+                        GraphicUrl = ad.GraphicUrl
                     };
-
                     return View("_Ad", adModel);
                 }
             }
-
             // no running ads found, render ad placeholder instead
             return RenderPlaceholder();
         }
