23a9e0f75f5a65fbcaff011fff934867d2700f24
PuttItOut <github.com@not.mrperfect.us>
2016-05-24 22:15:18 -0700

    Setting Based Thresholds

--- Voat/Voat.Business/Rules/Posting/PostCommentCCPRule.cs
+++ Voat/Voat.Business/Rules/Posting/PostCommentCCPRule.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Voat.Configuration;
 using Voat.RulesEngine;
 
 namespace Voat.Rules.Posting
@@ -10,7 +11,7 @@ namespace Voat.Rules.Posting
     [RuleDiscovery("Approved if user who has -50 CCP or less has submitted fewer than 5 comments in a 24 hour sliding window.", "approved = (user.CCP <= -50 and TotalCommentsInPast24Hours < 5 or user.CCP > -50)")]
     public class PostCommentCCPRule : MinimumCCPRule
     {
-        private int countThreshold = 5;
+        private int countThreshold = Settings.DailyCommentPostingQuotaForNegativeScore;
 
         public PostCommentCCPRule() : base("Comment CCP Throttle", "6.0", -50, RuleScope.PostComment)
         {
--- Voat/Voat.Business/Rules/Posting/PostSubmissionCCPRule.cs
+++ Voat/Voat.Business/Rules/Posting/PostSubmissionCCPRule.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Voat.Configuration;
 using Voat.RulesEngine;
 
 namespace Voat.Rules.Posting
@@ -11,7 +12,7 @@ namespace Voat.Rules.Posting
     [RuleDiscovery("Approved if user who has -50 CCP or less has submitted fewer than 1 submission in a 24 hour sliding window.", "approved = (user.CCP <= -50 and TotalSubmissionsInPast24Hours < 1 or user.CCP > -50)")]
     public class PostSubmissionCCPRule : MinimumCCPRule
     {
-        private int countThreshold = 1;
+        private int countThreshold = Settings.DailyPostingQuotaForNegativeScore;
 
         public PostSubmissionCCPRule() : base("Submission CCP Throttle", "6.1", -50, RuleScope.PostSubmission)
         {
@@ -19,11 +20,12 @@ namespace Voat.Rules.Posting
 
         protected override RuleOutcome EvaluateRule(VoatRuleContext context)
         {
-            //TODO: Need implementation of this rule
-            //if (context.UserInformation.CommentPoints.Sum <= MinimumCommentPoints && context.TotalSubmissionsInPast24Hours >= countThreshold)
-            //{
-            //    return CreateOutcome(RuleResult.Denied, "User with CCP value of {0} is limited to {1} submission(s) in 24 hours.", context.UserInformation.CommentPoints.Sum, countThreshold);
-            //}
+            //TODO:
+            //Need implementation of this rule
+            if (context.UserData.Information.CommentPoints.Sum <= MinimumCommentPoints && context.UserData.TotalVotesUsedIn24Hours >= countThreshold)
+            {
+                return CreateOutcome(RuleResult.Denied, "User with CCP value of {0} is limited to {1} posts(s) in 24 hours.", context.PropertyBag.CCP, countThreshold);
+            }
             return base.EvaluateRule(context);
         }
     }
