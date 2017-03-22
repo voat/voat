1f08c328a99a56c1f1418f70e1a6b967524de939
PuttItOut <puttitout@voat.co>
2017-03-22 12:42:56 -0700

    Gulp Build Sass

---
+++ Voat/Voat.UI/gulpfile.js
@@ -0,0 +1,25 @@
+﻿/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var sass = require("gulp-sass");
+
+
+gulp.task('default', function () {
+    // place code for your default task here
+    alert("hi");
+});
+
+
+
+var paths = {
+    root: "./Content/"
+}
+paths.scss = paths.root + "Sass/**/*.scss";
+gulp.task('sass', function () {
+    gulp.src(paths.scss)
+        .pipe(sass())
+        .pipe(gulp.dest(paths.root));
+});
