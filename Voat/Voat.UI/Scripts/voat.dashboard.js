3fea07d3add661d52178feb6f6716ae1795a69f8
PuttItOut <puttitout@voat.co>
2017-05-18 17:13:14 -0700

    UI Views License Update (Part 1)

--- Voat/Voat.UI/Scripts/voat.dashboard.js
+++ Voat/Voat.UI/Scripts/voat.dashboard.js
@@ -1,8 +1,23 @@
 ﻿/*
+    
+    Copyright(c) Voat, Inc.
 
-Now these lines technically are no longer blank. ;)
+    This file is part of Voat.
+
+    This source file is subject to version 3 of the GPL license,
+    that is bundled with this package in the file LICENSE, and is
+    available online at http://www.gnu.org/licenses/gpl-3.0.txt;
+    you may not use this file except in compliance with the License.
+
+    Software distributed under the License is distributed on an
+    "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express
+    or implied. See the License for the specific language governing
+    rights and limitations under the License.
+
+    All Rights Reserved.
 
 */
+
 function closeDashboardHandler(e) {
 
     //StyleSheet Preview loads two full pages (yeah yeah I already know), so this selector allows all the elements with id #dashboard to be selected
--- Voat/Voat.UI/Scripts/voat.ui.js
+++ Voat/Voat.UI/Scripts/voat.ui.js
@@ -1,4 +1,24 @@
-﻿//Voat UI JS framework - Version 0.8beta - 12/09/2014
+﻿/*
+    
+    Copyright(c) Voat, Inc.
+
+    This file is part of Voat.
+
+    This source file is subject to version 3 of the GPL license,
+    that is bundled with this package in the file LICENSE, and is
+    available online at http://www.gnu.org/licenses/gpl-3.0.txt;
+    you may not use this file except in compliance with the License.
+
+    Software distributed under the License is distributed on an
+    "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express
+    or implied. See the License for the specific language governing
+    rights and limitations under the License.
+
+    All Rights Reserved.
+
+*/
+
+//Voat UI JS framework - Version 0.8beta - 12/09/2014
 //Tested only with the latest version of IE, FF, & Chrome
 
 var UI = window.UI || {};
